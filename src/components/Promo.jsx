66116cca6d80363088d051310768c4187d524c78
Enrique1311 <ejspinelli131176@gmail.com>
2022-09-27 22:58:59 -0300

    Se agregó la sección Promo

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import promo from "../assets/promo.png";
 
 const Promo = () => {
-	return <div>Promo</div>;
+	return (
+		<div className="promo-container">
+			<div className="container mx-3 py-3">
+				<div className="content">
+					<div className="title-container">
+						<h2>No te pierdas Nuestras Promociones</h2>
+						<p>
+							Siempre damos a nuestros clientes subscriptos una promoción.
+							¡Subscríbete ahora!
+						</p>
+					</div>
+					<div className="subscribe-container">
+						<input type="email" placeholder="yourname@mail.com" />
+						<button>Subscribirme</button>
+					</div>
+				</div>
+				<div className="promo-image">
+					<img src={promo} alt="Promo" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Promo;
