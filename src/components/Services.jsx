9cebbb521698f8e942977cfef495673ab91cce87
Enrique1311 <ejspinelli131176@gmail.com>
2023-02-14 20:39:10 -0300

    Modificación de botones

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,53 +1,67 @@
-import React from "react";
-import { HiArrowSmRight } from "react-icons/hi";
-import service1 from "../assets/service1.png";
-import service2 from "../assets/service2.png";
-import service3 from "../assets/service3.png";
-import service4 from "../assets/service4.png";
+import React from 'react';
+import { HiArrowSmRight } from 'react-icons/hi';
+import service1 from '../assets/service1.png';
+import service2 from '../assets/service2.png';
+import service3 from '../assets/service3.png';
+import service4 from '../assets/service4.png';
 
 function Services() {
 	return (
-		<div className="services-container mx-3 py-3">
-			<div className="container">
-				<div className="service one">
+		<div className='services-container mx-3 py-3'>
+			<div className='container'>
+				<div className='service one'>
 					<p>Oferta de la Semana</p>
 					<h4>Envío Gratis</h4>
-					<button>Conoce más...</button>
-					<div className="image">
-						<img src={service1} alt="service" />
+					<button>
+						Conoce más <HiArrowSmRight />
+					</button>
+					<div className='image'>
+						<img
+							src={service1}
+							alt='service'
+						/>
 					</div>
 				</div>
-				<div className="service two">
-					<div className="content">
+				<div className='service two'>
+					<div className='content'>
 						<h4>Hasta 50% de descuento</h4>
-						<p>
+						<button>
 							Conoce más <HiArrowSmRight />
-						</p>
+						</button>
 					</div>
-					<div className="image">
-						<img src={service2} alt="service" />
+					<div className='image'>
+						<img
+							src={service2}
+							alt='service'
+						/>
 					</div>
 				</div>
-				<div className="service three">
-					<div className="image">
-						<img src={service3} alt="service" />
+				<div className='service three'>
+					<div className='image'>
+						<img
+							src={service3}
+							alt='service'
+						/>
 					</div>
-					<div className="content">
+					<div className='content'>
 						<h4>Promos Online</h4>
-						<p>
+						<button>
 							Conoce más <HiArrowSmRight />
-						</p>
+						</button>
 					</div>
 				</div>
-				<div className="service four">
-					<div className="image">
-						<img src={service4} alt="service" />
+				<div className='service four'>
+					<div className='image'>
+						<img
+							src={service4}
+							alt='service'
+						/>
 					</div>
 					<p>Ofertas de Aniversario</p>
 					<h4>Envío Gratis</h4>
-					<p>
+					<button>
 						Conoce más <HiArrowSmRight />
-					</p>
+					</button>
 				</div>
 			</div>
 		</div>
